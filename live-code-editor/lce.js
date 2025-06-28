136b9b0eda2afdd9474d5ef465c19ae375cb1bca
Quang Huy <ngquanghuy.work@gmail.com>
2025-06-28 18:49:36 +0700

    fix: context menu position, handle context menu options

--- live-code-editor/lce.js
+++ live-code-editor/lce.js
@@ -1,60 +1,82 @@
-const textarea = document.querySelector('#textarea')
-const preview = document.querySelector('#website-preview')
-const contextMenu = document.querySelector('.context-modal')
+const $ = document.querySelector.bind(document)
+
+const textarea = $('#textarea')
+const preview = $('#website-preview')
+const contextMenu = $('.context-modal')
 
 textarea.addEventListener('input', renderHtml)
 window.addEventListener('beforeunload', handleBeforeUnload)
-textarea.addEventListener('contextmenu', handleContextMenu)
-document.addEventListener('mousedown', hideMenuWhenMouseDown)
+document.addEventListener('contextmenu', handleContextMenu)
+document.addEventListener('mousedown', handleMouseDown)
 
-
+//render html in iframe by textarea input
 function renderHtml() {
     preview.srcdoc = textarea.value.trim()
 }
 
+//handle when unload
 function handleBeforeUnload(e) {
     if (!textarea.value.trim()) return
-    e.returnValue = 'message'
+    e.preventDefault()
+    e.returnValue = true
 }
 
+//handle context menu
 function handleContextMenu(e) {
     e.preventDefault()
-
-    handleMenuPosition(e)
     showMenu()
-    handleOption()
+    handleMenuPosition(e)
+    handleOptions()
 }
 
+//handle context menu position
+function handleMenuPosition(e) {
+    contextMenu.style.top = `${(e.pageY + contextMenu.offsetHeight) > innerHeight ? //if X click point + menu height > window height
+        innerHeight - contextMenu.offsetHeight : //then return window height - menu height
+        e.pageY}px` //else reuturn X click point
+
+    contextMenu.style.left = `${e.pageX + contextMenu.offsetWidth > innerWidth ?
+        innerWidth - contextMenu.offsetWidth :
+        e.pageX}px`
+}
+
+//show context menu
 function showMenu() {
     contextMenu.classList.add('show')
 }
 
-function handleMenuPosition(e) {
-    contextMenu.style.top = `${
-        (e.pageY + contextMenu.offsetHeight) > innerHeight ?
-        e.pageY - contextMenu.offsetHeight :
-        e.pageY}px`
-    contextMenu.style.left = `${e.pageX}px`
+function hideMenu() {
+    contextMenu.classList.remove('show')
 }
 
-function handleOption() {
-    function selectAll() {
+//handle options of context menu
+function handleOptions() {
+    const selectAllBtn = contextMenu.querySelector('.context--select-all')
+    const clearBtn = contextMenu.querySelector('.context--clear')
+
+    selectAllBtn.addEventListener('click', selectAll)
+    clearBtn.addEventListener('click', clear)
+
+    //select all the code
+    function selectAll(e) {
+        e.preventDefault()
         textarea.focus()
         textarea.select()
+        hideMenu()
     }
 
-    function clear() {
+    //clear all the code
+    function clear(e) {
+        e.preventDefault()
         textarea.value = ''
         preview.srcdoc = ''
+        hideMenu()
     }
-
-    const selectBtn = contextMenu.querySelector('.context--select-all')
-    selectBtn.onclick = () => selectAll()
-
-    const clearBtn = contextMenu.querySelector('.context--clear')
-    clearBtn.onclick = () => clear()
 }
 
-function hideMenuWhenMouseDown() {
-    contextMenu.classList.remove('show')
+//hide current context menu when mouse down
+function handleMouseDown(e) {
+    // if (!contextMenu.contains(e.target)) showMenu()
+    console.log(e.target)
+    if (!contextMenu.contains(e.target)) hideMenu()
 }
